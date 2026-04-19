48409b7c41e3af8b5091b9991769c0345a2ec56e
cmdtvt <tvtuusa@gmail.com>
2026-04-19 18:29:28 +0300

    firebaseConfig mess

---
+++ frontend/src/authService.ts
@@ -0,0 +1,50 @@
+import {
+  getAuth, 
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  type User,
+  type UserCredential
+} from "firebase/auth";
+
+// import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig"
+
+// const app = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseConfig);
+/**
+ * Kirjautuminen sähköposti + salasana
+ */
+export const loginWithEmail = async (
+  email: string,
+  password: string
+): Promise<User> => {
+  const result: UserCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return result.user;
+};
+
+/**
+ * Käyttäjän rekisteröinti (Ei tässä tehtävässä välttämätön)
+ */
+export const registerWithEmail = async (
+  email: string,
+  password: string
+): Promise<User> => {
+const result: UserCredential = await createUserWithEmailAndPassword(
+  auth,
+  email,
+  password
+);
+return result.user;
+};
+
+/**
+ * Kirjautuminen ulos
+ */
+export const logout = async (): Promise<void> => {
+  await signOut(auth);
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
+import App from './App.tsx'
 import CodeName from './CodeName.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CodeName></CodeName>
+    {/* <CodeName></CodeName> */}
+    <App></App>
   </StrictMode>,
 )
