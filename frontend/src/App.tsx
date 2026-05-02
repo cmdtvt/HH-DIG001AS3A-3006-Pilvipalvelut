5d33a1199158fb8e1ac563183c78f6fa6bbd3f85
cmdtvt <tvtuusa@gmail.com>
2026-05-02 22:15:42 +0300

    cleaning & setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,87 +1,79 @@
-import { useEffect, useState } from "react";
-import { onAuthStateChanged, type User } from "firebase/auth";
-import LoginForm from "./LoginForm";
-import { auth, logout } from "./authService";
+import { useState, useEffect } from 'react'
+
+import './App.css'
+import LoginForm from './LoginForm';
+import { auth, logout } from './authService';
+import { onAuthStateChanged, type User } from 'firebase/auth';
+// import { getOrCreateCodename } from './codenameService';
+import { QuizForm } from './components/QuizForm';
+// import { createSession } from './gameSessionService';
+// import { resolveRound } from './gameController';
+import { type Session } from './types/Session';
+import { type Player } from './types/Player';
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [codename, setCodename] = useState<string>("");
   const [user, setUser] = useState<User | null>(null);
-  const [nimi, setNimi] = useState("");
 
+  // Initialize count from localStorage on mount
   useEffect(() => {
-    let ladattu = localStorage.getItem("codename")
-    if (ladattu !== null) {
-      setNimi(ladattu)
+    const storedCount = localStorage.getItem('laskuri');
+    if (storedCount) {
+      setCount(parseInt(storedCount, 10));
     }
-  }, []);
 
-  let eka = [
-    "Kenraali",
-    "Tohtori",
-    "Mestari",
-    "Kalkkuna",
-    "Presidentti",
-    "Eversti",
-    "Suomen",
-    "Rehtori"
-  ]
-
-  let toka = [
-    "Kalkkuna",
-    "Pelaaja",
-    "Voima",
-    "Voittaja",
-    "Ilmoittaja",
-    "Majuri",
-    "Tohtori Mestari",
-    "Kokki",
-    "Bruutus",
-    "Murskaaja",
-    "Purkaja",
-    "Kehittäjä",
-    "Golffari",
-  ]
-
-
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
       setUser(firebaseUser);
+      const name = getOrCreateCodename(firebaseUser!.uid);
+      setCodename(name);
+      setSession(await createSession({ name: name + "n-arvaussessio", creatorName: name }));
     });
 
-    return () => unsubscribe();
-  }, []);
+    return () => unsubscribe();   
+  }, [])
 
 
-  useEffect(() => {
-    if (!user) return;
-
-    const key = "codename_"+user.uid;
-    const saved = localStorage.getItem(key);
-
-    if (saved) {
-      setNimi(saved);
-    } else {
-      let gen_name = eka[Math.floor(Math.random() * eka.length)] +" " + toka[Math.floor(Math.random() * toka.length)];
-
-      localStorage.setItem(key, gen_name);
-      setNimi(gen_name);
+  function submitGuess(guess: number): void {
+    if ( session ) {
+      try {
+        resolveRound(session, guess);
+      } catch (error) {
+        console.error("Kierroksen luonti epäonnistui: ", error);
+      }  
     }
-  }, [user]);
+  }
 
   return (
+    <>
+      <section id="center">
 
-    <div>
-      {user ? (
-        <>
-          <p>👋 Tervetuloa, {nimi || user.email}</p>
-          <button onClick={logout}>Kirjaudu ulos</button>
-        </>
-      ) : (
-        <LoginForm />
-      )}
-    </div>
-  );
+        <div>
+          <h1>Heippa maailma!</h1>
+        </div>
 
+        <div>
+          {user ? (
+            <>
+              <p>👋 Tervetuloa, {codename}</p>
+              <button onClick={logout}>Kirjaudu ulos</button>
+            </>
+          ) : (
+            <LoginForm />
+          )}
+        </div>
+
+        <div>
+          <QuizForm 
+            onSubmitGuess={(guess) => submitGuess(guess)} 
+            players={[]} 
+            currentUserId={codename}
+          />
+        </div>
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/types/Session.ts
@@ -0,0 +1,16 @@
+
+// import type { Player } from "./Player";
+
+export type SessionStatus = "waiting" | "playing" | "finished";
+
+export type Session = {
+    id: string;
+    sessionName: string;
+    scores: Record<string, number>; // playerId -> score
+    currentRound: number;
+    currentGame?: string;
+    status: SessionStatus;
+    createdAt: unknown;
+    createdBy: unknown;
+    lastActivity?: unknown;
+};
